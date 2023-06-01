42efc54964772b989a1fff8bd334ee18cdf42151
kev0921 <kevin.hu0921@gmail.com>
2023-06-01 18:54:06 -0400

    Add all necessary files to the repo

---
+++ src/Navbar.js
@@ -0,0 +1,17 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>Kevin Hu</h1>
+            <div className="links">
+                <a href="/home">Home</a>
+                <a href="/projects">Projects</a>
+                <a href="/experiences">Experiences</a>
+                <a href="/about">About</a>
+                <a href="/contact">Contact</a>
+            </div>
+            <a href="google.ca"></a>
+        </nav>
+     );
+}
+ 
+export default Navbar;
